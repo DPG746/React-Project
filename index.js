53648c7869e3a25fb98907b915ebd3a15dcfd696
DPG746 <104110048+DPG746@users.noreply.github.com>
2023-04-01 17:17:23 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+//const h1=document.createElement("h1")
+//h1.textContent="This is an imperative way to program"
+//h1.className="header"
+//document.getElementById("root").append(h1)
+const page=(
+    <nav>
+        <h1>this is a react project</h1>
+        <ul>
+            <li>About</li>
+            <li>Contact</li>
+            <li>Instant help</li>
+        </ul>
+    </nav>
+)
+
+ReactDOM.render(page, document.getElementById("root"))
